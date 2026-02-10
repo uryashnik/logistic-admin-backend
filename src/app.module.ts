78579c4bb993e5bd9e61e8f9ade723ac3e0092be
uriashnyk.vitalii <vuryashnik@unity-bars.com>
2026-02-10 20:49:17 +0100

    add typeorm and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PostgresModule } from './common/postgres/postgres.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: [`.env`] })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: [`.env`] }), PostgresModule.register()],
 })
 export class AppModule {}
